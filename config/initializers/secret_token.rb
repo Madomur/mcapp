7a69e13950c605b754395a64dcc02a589f5401bc
Martin Delbrück <info@martindelbrueck.de>
2011-11-23 11:22:51 +0100

    * create rails3.1 app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mcapp::Application.config.secret_token = 'ea0b874006ca5e861c2dedb7aa03edad397425760f72fc7fafc5b938efb47daed50752c954001e48498d022e299598413df8b506fe165f21d0df3e55c7b626a3'
